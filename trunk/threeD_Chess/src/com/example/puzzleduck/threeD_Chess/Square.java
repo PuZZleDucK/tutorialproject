83990cf6afcb55031911d2a51d769826593fd615
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2010-11-22 06:04:24 +0000

    NeHe: Tutorial03 - piece of cake, even enhanced square :)

--- trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Square.java
+++ trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Square.java
@@ -9,12 +9,19 @@ import javax.microedition.khronos.opengles.GL10;
 public class Square {
 
 	private static final int BYTE_SIZE = 4;
-	private FloatBuffer vertexBuffer;
+	private FloatBuffer vertexBuffer, colorBuffer;
 	private float vertices[] ={
 			-1.0f, -1.0f, 0.0f, //Bottom Left
 			1.0f, -1.0f, 0.0f, 	//Bottom Right
 			-1.0f, 1.0f, 0.0f, 	//Top Left
 			1.0f, 1.0f, 0.0f 	//Top Right
+	};
+	
+	private float colors[] ={
+			0.0f, 0.0f, 0.0f, 1.0f, 	//Bottom Left	k
+			0.0f, 1.0f, 0.0f, 1.0f, 	//Bottom Right	g
+			0.0f, 0.0f, 1.0f, 1.0f, 	//Top Left		b
+			0.0f, 0.0f, 0.0f, 1.0f		//Top Right		k
 	};
 	
 	public Square()
@@ -25,6 +32,12 @@ public class Square {
 		vertexBuffer.put(vertices);
 		vertexBuffer.position(0);
 		
+		bBuff = ByteBuffer.allocateDirect(colors.length * BYTE_SIZE);
+		bBuff.order(ByteOrder.nativeOrder());
+		colorBuffer = bBuff.asFloatBuffer();
+		colorBuffer.put(colors);
+		colorBuffer.position(0);
+		
 	}
 	
 	//Self Rendering
@@ -34,12 +47,18 @@ public class Square {
 		gl.glFrontFace(GL10.GL_CW);
 		//point to buffer
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+		//set color matrix
+		gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
 		//enable buffer
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+//		//set color: blue
+//		gl.glColor4f(0.5f, 0.5f, 0.5f, 1.0f);
 		//draw triangles
 		gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length/3);
 		//disable client state
 		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
 		
 	}	
 	
