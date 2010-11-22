0901ed7f962da81d3a814f8e056644163767a43e
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2010-11-22 03:57:40 +0000

    Need more GL experience ... NeHe Tutorials: Tutorial 02

--- trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/threeD_Chess.java
+++ trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/threeD_Chess.java
@@ -1,22 +1,43 @@
 package com.example.puzzleduck.threeD_Chess;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class threeD_Chess extends Activity {
     
-//	private static final String LOG_TAG = threeD_Chess.class.getSimpleName();
-	private threeD_Chess_view _threeD_Chess_view;
+	//Decided I don;t have enough practice with open GL...
+	// T4: going through the NeHe tutorials, they seem to almost be a standard
+	// and have model loading in a tutorial
+	// Props: http://www.insanitydesign.com/
+	
+	private GLSurfaceView _threeD_Chess_view;
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        _threeD_Chess_view = new threeD_Chess_view(this);
+//        _threeD_Chess_view = new threeD_Chess_view(this);
+        _threeD_Chess_view = new GLSurfaceView(this);
+
+        //NeHe tutorial: adding custom renderer:
+        _threeD_Chess_view.setRenderer(new threeD_Renderer());
         setContentView(_threeD_Chess_view);
-        
-        
-        //setContentView(R.layout.main);
     }
+    
+    //NeHe Tutorial: adding pause and resume:
+    @Override
+    protected void onResume(){
+    	super.onResume();
+    	_threeD_Chess_view.onResume();
+    }
+    
+    @Override
+    protected void onPause(){
+    	super.onPause();
+    	_threeD_Chess_view.onPause();
+    }
+    
+    
+    
 }
