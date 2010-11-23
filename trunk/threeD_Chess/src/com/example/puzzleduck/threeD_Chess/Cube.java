69518055e26621a267e8d7bc6eb35d275a7a2281
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2010-11-23 23:04:30 +0000

    nehe7 - Lights and UI

--- trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Cube.java
+++ trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Cube.java
@@ -7,6 +7,7 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.GL11;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -15,82 +16,118 @@ import android.opengl.GLUtils;
 
 public class Cube {
 	private static final int BYTE_SIZE = 4;
-	private FloatBuffer vertexBuffer, textureBuffer;
+	private FloatBuffer vertexBuffer, textureBuffer, normalBuffer;
 	private ByteBuffer indexBuffer;
 	
-	private int[] textures = new int[1];
+	private int[] textures = new int[3];
 	
 	private float vertices[] ={
-    		-1.0f, -1.0f, 1.0f, //Vertex 0
-    		1.0f, -1.0f, 1.0f,  //v1
-    		-1.0f, 1.0f, 1.0f,  //v2
-    		1.0f, 1.0f, 1.0f,   //v3
-    		
-    		1.0f, -1.0f, 1.0f,	//...
-    		1.0f, -1.0f, -1.0f,    		
-    		1.0f, 1.0f, 1.0f,
-    		1.0f, 1.0f, -1.0f,
-    		
-    		1.0f, -1.0f, -1.0f,
-    		-1.0f, -1.0f, -1.0f,    		
-    		1.0f, 1.0f, -1.0f,
-    		-1.0f, 1.0f, -1.0f,
-    		
-    		-1.0f, -1.0f, -1.0f,
-    		-1.0f, -1.0f, 1.0f,    		
-    		-1.0f, 1.0f, -1.0f,
-    		-1.0f, 1.0f, 1.0f,
-    		
-    		-1.0f, -1.0f, -1.0f,
-    		1.0f, -1.0f, -1.0f,    		
-    		-1.0f, -1.0f, 1.0f,
-    		1.0f, -1.0f, 1.0f,
-    		
-    		-1.0f, 1.0f, 1.0f,
-    		1.0f, 1.0f, 1.0f,    		
-    		-1.0f, 1.0f, -1.0f,
-    		1.0f, 1.0f, -1.0f
+			-1.0f, -1.0f, 1.0f, //v0
+			1.0f, -1.0f, 1.0f, 	//v1
+			-1.0f, 1.0f, 1.0f, 	//v2
+			1.0f, 1.0f, 1.0f, 	//v3
+
+			1.0f, -1.0f, 1.0f, 	// ...
+			1.0f, -1.0f, -1.0f, 
+			1.0f, 1.0f, 1.0f, 
+			1.0f, 1.0f, -1.0f,
+
+			1.0f, -1.0f, -1.0f, 
+			-1.0f, -1.0f, -1.0f, 
+			1.0f, 1.0f, -1.0f, 
+			-1.0f, 1.0f, -1.0f,
+
+			-1.0f, -1.0f, -1.0f, 
+			-1.0f, -1.0f, 1.0f, 
+			-1.0f, 1.0f, -1.0f, 
+			-1.0f, 1.0f, 1.0f,
+
+			-1.0f, -1.0f, -1.0f, 
+			1.0f, -1.0f, -1.0f, 
+			-1.0f, -1.0f, 1.0f, 
+			1.0f, -1.0f, 1.0f,
+
+			-1.0f, 1.0f, 1.0f, 
+			1.0f, 1.0f, 1.0f, 
+			-1.0f, 1.0f, -1.0f, 
+			1.0f, 1.0f, -1.0f, 
 	};
 	
+	private float normals[] ={
+			// Normals
+			0.0f, 0.0f, 1.0f, 						
+			0.0f, 0.0f, -1.0f, 
+			0.0f, 1.0f, 0.0f, 
+			0.0f, -1.0f, 0.0f, 
+			
+			0.0f, 0.0f, 1.0f, 
+			0.0f, 0.0f, -1.0f, 
+			0.0f, 1.0f, 0.0f, 
+			0.0f, -1.0f, 0.0f,
+			
+			0.0f, 0.0f, 1.0f, 
+			0.0f, 0.0f, -1.0f, 
+			0.0f, 1.0f, 0.0f, 
+			0.0f, -1.0f, 0.0f,
+			
+			0.0f, 0.0f, 1.0f, 
+			0.0f, 0.0f, -1.0f, 
+			0.0f, 1.0f, 0.0f, 
+			0.0f, -1.0f, 0.0f,
+			
+			0.0f, 0.0f, 1.0f, 
+			0.0f, 0.0f, -1.0f, 
+			0.0f, 1.0f, 0.0f, 
+			0.0f, -1.0f, 0.0f,
+			
+			0.0f, 0.0f, 1.0f, 
+			0.0f, 0.0f, -1.0f, 
+			0.0f, 1.0f, 0.0f, 
+			0.0f, -1.0f, 0.0f,
+	};
+	
+	
 	private float texture[] ={	
-    		0.0f, 0.0f,
-    		0.0f, 1.0f,
-    		1.0f, 0.0f,
-    		1.0f, 1.0f, 
-    		
-    		0.0f, 0.0f,
-    		0.0f, 1.0f,
-    		1.0f, 0.0f,
-    		1.0f, 1.0f,
-    		
-    		0.0f, 0.0f,
-    		0.0f, 1.0f,
-    		1.0f, 0.0f,
-    		1.0f, 1.0f,
-    		
-    		0.0f, 0.0f,
-    		0.0f, 1.0f,
-    		1.0f, 0.0f,
-    		1.0f, 1.0f,
-    		
-    		0.0f, 0.0f,
-    		0.0f, 1.0f,
-    		1.0f, 0.0f,
-    		1.0f, 1.0f,
-    		
-    		0.0f, 0.0f,
-    		0.0f, 1.0f,
-    		1.0f, 0.0f,
-    		1.0f, 1.0f,
+			//Mapping coordinates for the vertices
+			0.0f, 0.0f, 
+			0.0f, 1.0f, 
+			1.0f, 0.0f, 
+			1.0f, 1.0f,
+
+			0.0f, 0.0f,
+			0.0f, 1.0f, 
+			1.0f, 0.0f,
+			1.0f, 1.0f,
+
+			0.0f, 0.0f, 
+			0.0f, 1.0f, 
+			1.0f, 0.0f, 
+			1.0f, 1.0f,
+
+			0.0f, 0.0f, 
+			0.0f, 1.0f, 
+			1.0f, 0.0f, 
+			1.0f, 1.0f,
+
+			0.0f, 0.0f, 
+			0.0f, 1.0f, 
+			1.0f, 0.0f, 
+			1.0f, 1.0f,
+
+			0.0f, 0.0f, 
+			0.0f, 1.0f, 
+			1.0f, 0.0f, 
+			1.0f, 1.0f, 
 	};
 	
 	private byte indices[] = {
-    		0,1,3, 0,3,2, 			//Face front
-    		4,5,7, 4,7,6, 			//Face right
-    		8,9,11, 8,11,10, 		//... 
-    		12,13,15, 12,15,14, 	
-    		16,17,19, 16,19,18, 	
-    		20,21,23, 20,23,22, 	
+			// Faces definition
+			0, 1, 3, 0, 3, 2, 		// Face front
+			4, 5, 7, 4, 7, 6, 		// Face right
+			8, 9, 11, 8, 11, 10, 	// ...
+			12, 13, 15, 12, 15, 14, 
+			16, 17, 19, 16, 19, 18, 
+			20, 21, 23, 20, 23, 22, 
 	};
 	
 	public Cube()
@@ -100,12 +137,18 @@ public class Cube {
 		vertexBuffer = bBuff.asFloatBuffer();
 		vertexBuffer.put(vertices);
 		vertexBuffer.position(0);
-		
+
 		bBuff = ByteBuffer.allocateDirect(texture.length * BYTE_SIZE);
 		bBuff.order(ByteOrder.nativeOrder());
 		textureBuffer = bBuff.asFloatBuffer();
 		textureBuffer.put(texture);
 		textureBuffer.position(0);
+
+		bBuff = ByteBuffer.allocateDirect(normals.length * BYTE_SIZE);
+		bBuff.order(ByteOrder.nativeOrder());
+		normalBuffer = bBuff.asFloatBuffer();
+		normalBuffer.put(normals);
+		normalBuffer.position(0);
 		
 		indexBuffer = ByteBuffer.allocateDirect(indices.length);
 		indexBuffer.put(indices);
@@ -114,19 +157,21 @@ public class Cube {
 	}
 	
 	//Self Rendering
-	public void draw(GL10 gl)
+	public void draw(GL10 gl, int filter)
 	{
-		gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[filter]);
 		
 		//enable buffers
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
 		gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		gl.glEnableClientState(GL10.GL_NORMAL_ARRAY);
 		
 		//face rotation
 		gl.glFrontFace(GL10.GL_CW);
 		//point to buffers
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
 		gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+		gl.glNormalPointer(GL10.GL_FLOAT, 0, normalBuffer);
 
 		//draw triangles
 		gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_BYTE, indexBuffer);
@@ -134,11 +179,12 @@ public class Cube {
 		//disable client state
 		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
 		gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		gl.glDisableClientState(GL10.GL_NORMAL_ARRAY);
 		
 	}
 
 	public void loadGLTexture(GL10 gl, Context context) {
-		InputStream inStream = context.getResources().openRawResource(R.drawable.ehne);
+		InputStream inStream = context.getResources().openRawResource(R.drawable.crate);
 		Bitmap bitmap = null;
 		
 		try
@@ -155,18 +201,25 @@ public class Cube {
 		}
 		
 		//gen tex and bind to array
-		gl.glGenTextures(1, textures, 0);
+		gl.glGenTextures(3, textures, 0);
+
+		//create NEAREST tex and bind to array
 		gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
-		
-		//create filtered tex
 		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
-		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
-		//other filters
-		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
-		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
-		
-		//specify image for bitmap
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_NEAREST);
 		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+		//create LINEAR tex and bind to array
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+		//create MIPMAP tex and bind to array
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR_MIPMAP_NEAREST);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL11.GL_GENERATE_MIPMAP, GL10.GL_LINEAR_MIPMAP_NEAREST);
+		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
+		
 		bitmap.recycle();
 		
 	}	
--- trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/threeD_Renderer.java
+++ trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/threeD_Renderer.java
@@ -1,79 +1,129 @@
 package com.example.puzzleduck.threeD_Chess;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
+import android.opengl.GLSurfaceView;
 import android.opengl.GLSurfaceView.Renderer;
 import android.opengl.GLU;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 
-public class threeD_Renderer implements Renderer {
+public class threeD_Renderer extends GLSurfaceView implements Renderer {
 	
-	//NeHe: Adding new object classes
-//	private Triangle triangle;
-//	private Square square;
+	private static final int BYTE_SIZE = 4;
 
-//	private Pyramid pyramid;
 	private Cube cube;
+
+	private float xRot, yRot;
+	private float xSpeed, ySpeed;
+	private float depth = -5.0f;
 	
-	private float xRot, yRot, zRot;
-//	private float sqrRot, triRot = 0;
+	private int filter;
+	
+	private boolean light = false;
+
+	private float[] lightAmbient = {0.5f, 0.5f, 0.5f, 1.0f,};
+	private float[] lightDiffuse = {1.0f, 1.0f, 1.0f, 1.0f,};
+	private float[] lightPosition = {0.0f, 0.0f, 2.0f, 1.0f,};
+	
+	private FloatBuffer lightAmbientBuffer, lightDiffuseBuffer, lightPositionBuffer;
+	
+	private float oldX, oldY;
+	
+	private final float TOUCH_SCALE = 0.2f;
 	
 	private Context context;
 	
 	
 	public threeD_Renderer(Context context){
+		super(context);
+		this.setRenderer(this);
+		
+		this.requestFocus();
+		this.setFocusableInTouchMode(true);
 		this.context = context;
+		
+		ByteBuffer bBuff = ByteBuffer.allocateDirect(lightAmbient.length * BYTE_SIZE);
+		bBuff.order(ByteOrder.nativeOrder());
+		lightAmbientBuffer = bBuff.asFloatBuffer();
+		lightAmbientBuffer.put(lightAmbient);
+		lightAmbientBuffer.position(0);
+
+		bBuff = ByteBuffer.allocateDirect(lightDiffuse.length * BYTE_SIZE);
+		bBuff.order(ByteOrder.nativeOrder());
+		lightDiffuseBuffer = bBuff.asFloatBuffer();
+		lightDiffuseBuffer.put(lightDiffuse);
+		lightDiffuseBuffer.position(0);
+
+		bBuff = ByteBuffer.allocateDirect(lightPosition.length * BYTE_SIZE);
+		bBuff.order(ByteOrder.nativeOrder());
+		lightPositionBuffer = bBuff.asFloatBuffer();
+		lightPositionBuffer.put(lightPosition);
+		lightPositionBuffer.position(0);
+		
+		
 		cube = new Cube();
 	}
 	
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 
-		cube.loadGLTexture(gl, this.context);
+		//lights:
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_AMBIENT, lightAmbientBuffer);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, lightDiffuseBuffer);
+		gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, lightPositionBuffer);
+		gl.glEnable(GL10.GL_LIGHT0);
+		
+		gl.glDisable(GL10.GL_DITHER);
 		gl.glEnable(GL10.GL_TEXTURE_2D);
 		
 		gl.glShadeModel(GL10.GL_SMOOTH);
-		//Define "clipping wall"/clear color - lower transparency than origional?
+		
 		gl.glClearColor(0f, 0f, 0f, 0.5f);
-		//Clear depth
 		gl.glClearDepthf(1.0f);
 		gl.glEnable(GL10.GL_DEPTH_TEST);//checking z-order
 		gl.glDepthFunc(GL10.GL_LEQUAL);
 		//perspective
 		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
 		
+		cube.loadGLTexture(gl, this.context);
 	
 	}
 	
 
 
 	public void onDrawFrame(GL10 gl) {
-		//NeHe:		
-		
-		//clear buffer and reset matrix
+		//NeHe:  clear buffer and reset matrix
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT| GL10.GL_DEPTH_BUFFER_BIT);
 		gl.glLoadIdentity();
 
+		if(light)
+		{
+			gl.glEnable(GL10.GL_LIGHTING);
+		}else
+		{
+			gl.glDisable(GL10.GL_LIGHTING);
+		}
 		
-		
-		gl.glTranslatef(0.0f, 0.0f, -5.0f);
+		gl.glTranslatef(0.0f, 0.0f, depth);
 		gl.glScalef(0.8f, 0.8f, 0.8f);
 		gl.glRotatef(xRot, 1.0f, 0.0f, 0.0f);
 		gl.glRotatef(yRot, 0.0f, 1.0f, 0.0f);
-		gl.glRotatef(zRot, 0.0f, 0.0f, 1.0f);
 		
-		cube.draw(gl);
+		cube.draw(gl, filter);
 		
-		xRot += 1.0f;
-		yRot += 1.5f;
-		zRot += 0.5f;
+		xRot += xSpeed;
+		yRot += ySpeed;
 						
 	}
 
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-//		_width = width;
-//		_height = height;
 		if( height == 0 )
 		{
 			height = 1;
@@ -88,5 +138,63 @@ public class threeD_Renderer implements Renderer {
 		gl.glLoadIdentity();
 		
 	}
+
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event) 
+	{
+		if( keyCode == KeyEvent.KEYCODE_DPAD_LEFT)
+		{
+			ySpeed -= 0.1f;
+		}else if( keyCode == KeyEvent.KEYCODE_DPAD_RIGHT)
+		{
+			ySpeed += 0.1f;
+		}else if( keyCode == KeyEvent.KEYCODE_DPAD_UP)
+		{
+			xSpeed -= 0.1f;
+		}else if( keyCode == KeyEvent.KEYCODE_DPAD_DOWN)
+		{
+			ySpeed += 0.1f;
+		}else if( keyCode == KeyEvent.KEYCODE_DPAD_CENTER)
+		{
+			filter += 1;
+			if(filter > 2){filter = 0;}
+		}
+		
+		return true;
+	}
+
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) 
+	{
+		float x = event.getX();
+		float y = event.getY();
+		if( event.getAction() == MotionEvent.ACTION_MOVE)
+		{
+			float deltaX = x - oldX;
+			float deltaY = y - oldY;
+			int upperArea = this.getHeight()/10;
+			
+			//zoom in top 10% of screen
+			if(y < upperArea)
+			{
+				depth -= deltaX * TOUCH_SCALE /2;
+			}else
+			{
+				//or rotate
+				xRot -= deltaY * TOUCH_SCALE;
+				yRot -= deltaX * TOUCH_SCALE;
+			}
+		}else if(event.getAction() == MotionEvent.ACTION_UP)
+		{
+			light = !light;
+		}
+		
+		oldX = x;
+		oldY = y;
+		return true;
+	}
+	
 	
 }
