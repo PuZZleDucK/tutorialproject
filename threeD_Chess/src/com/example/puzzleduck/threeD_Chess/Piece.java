74dd347ffe438372ea881e63decd39628e74b87d
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2010-11-27 07:16:13 +0000

    Implemented some more basics, variables... including first half of board init

---
+++ threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Piece.java
@@ -0,0 +1,28 @@
+package com.example.puzzleduck.threeD_Chess;
+
+//typedef struct
+//{
+//  Coord xyzPos;
+//  Colour bwSide;
+//  Title nName;
+//  unsigned bVisible :1,
+//           bHasMoved:1; /* For king, rook, pawn only */
+//} Piece;
+
+
+public class Piece {
+
+	public Piece(int type, int file, int rank, int level, int side) {
+//EXAMPLE USAGE
+//		Piece blackKing = new Piece(KING, //title/type
+//									1, //file
+//									1, //rank
+//									1, //level
+//									BLACK); //side?		
+
+	}
+
+	
+	
+	
+}
