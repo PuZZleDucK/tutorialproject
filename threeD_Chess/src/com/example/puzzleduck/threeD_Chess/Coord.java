8714660bbc407b210e5ba7498c7b2c48a36caf6c
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2010-12-01 02:13:43 +0000

    incomplete changes: started on piece moves and rules

---
+++ threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Coord.java
@@ -0,0 +1,22 @@
+package com.example.puzzleduck.threeD_Chess;
+
+//typedef struct
+//{
+//  unsigned
+//    xFile  :3,
+//    yRank  :3,
+//    zLevel :2;
+//} Coord;
+
+
+public class Coord {
+
+	public int xFile, yRank, zLevel;
+	
+	public Coord()
+	{
+		
+	}
+	
+	
+}
