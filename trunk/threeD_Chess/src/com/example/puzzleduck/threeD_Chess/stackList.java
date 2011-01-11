12c4d7f6f9bfe0cc1abad7b763158db3d602966a
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2011-01-11 07:01:27 +0000

    implemented stack list for AI

---
+++ trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/stackList.java
@@ -0,0 +1,27 @@
+package com.example.puzzleduck.threeD_Chess;
+
+import java.util.Stack;
+
+public class stackList {
+
+//	Local struct stackList
+//	{
+//	  stack *stacks[BEST_STACKS];
+//	  int   ratings[BEST_STACKS];
+//	} bestMoves = {
+//	{ NULL, NULL, NULL, NULL, NULL },
+//	{ INT_MIN, INT_MIN, INT_MIN, INT_MIN, INT_MIN }
+//	};
+//
+//	#define BEST_STACKS 5
+	public int BEST_STACKS = 5;
+
+public Stack stacks[] = {null,null,null,null,null};
+public int   ratings[] = {0,0,0,0,0};
+	
+//	public stackList()
+//	{
+////		stacks = ;
+//	}
+	
+}
