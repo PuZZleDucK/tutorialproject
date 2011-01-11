0e88259cd34db6442e32adc0194dcce17294f079
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2011-01-11 05:09:11 +0000

    renamed stack to stack_el: stack just needs wrappers in main file... stack items need their own class

--- trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/threeD_Chess.java
+++ trunk/threeD_Chess/src/com/example/puzzleduck/threeD_Chess/threeD_Chess.java
@@ -1,6 +1,7 @@
 package com.example.puzzleduck.threeD_Chess;
 
 import java.util.Random;
+import java.util.Stack;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -373,12 +374,6 @@ private String winString = "";
 //	 */
 
 	
-	//
-//	struct stack_el
-//	{
-//	  Move *mvt;
-//	  struct stack_el *below;
-//	};
 //
 //	typedef struct
 //	{
@@ -1972,7 +1967,125 @@ private String winString = "";
 //	#include "3Dc.h"
 //	#include "3DcErr.h"
 
+		  
+		  
+		//Global stack *
+	public Stack stack;		  
+		  
+//stack has its own class... but may only need wrappers???
+//StackNew(void)
+//{
+//stack *s;
+//
+//s = (stack *)malloc(sizeof(stack));
+//if (!CHECK( s != NULL ))
+//return NULL;
+//s->top = NULL;
+//s->nSize = 0;
+//return s;
+//}
+	public void StackNew()
+	{
+		stack = new Stack();
+	}
+
+
+
+//Global void
+	//StackDelete(stack *s)
+	public void StackDelete(Stack s)
+	{
+//while(StackPop(s) != NULL)
+//nop();
+//
+//free(s);
+		stack = null;
+		return;
+	}
+
 	
+	
+//Global void
+//StackPush(stack *s, const Move *newMove)
+//public void StackPush(stack s, Move newMove)
+//{
+//struct stack_el *newEl;
+//
+//newEl = (struct stack_el *)malloc(sizeof(struct stack_el));
+//if (!CHECK( newEl != NULL ))
+//return;
+//newEl->mvt = (Move *)malloc(sizeof(Move));
+//if (!CHECK( newEl->mvt != NULL ))
+//return;
+//memcpy(newEl->mvt, newMove, sizeof(Move));
+//newEl->below = s->top;
+//s->top = newEl;
+//s->nSize++;
+//return;
+//}
+//
+//Global Move *
+//StackPop(stack *s)
+//{
+//Move *oldMove;
+//struct stack_el *oldEl;
+//
+//if (s->top == NULL)
+//return NULL;
+//
+//oldMove = s->top->mvt;
+//oldEl = s->top;
+//s->top = s->top->below;
+//s->nSize--;
+//free(oldEl);
+//
+//return oldMove;
+//}
+//
+///* Don't delete returned value; it's still on the stack! */
+//Global Move *
+//StackPeek(stack *s, int numMoves)
+//{
+//struct stack_el *oldEl;
+//
+//if (numMoves >= s->nSize)
+//return NULL;
+//
+//for (oldEl = s->top; numMoves > 0; --numMoves)
+//oldEl = oldEl->below;
+//
+//return oldEl->mvt;
+//}
+//
+//#ifdef DEBUG
+//Global void
+//StackDump( stack *s )
+//{
+//int i;
+//struct stack_el *el;
+//
+//el = s->top;
+//for (i=0; i<s->nSize; ++i)
+//{
+//  printf("%i: %s at (%i,%i,%i) to (%i,%i,%i)\n",i,
+//         Piece2String( Board[el->mvt->xyzBefore.zLevel][el->mvt->xyzBefore.yRank][ el->mvt->xyzBefore.xFile] ),
+//         el->mvt->xyzBefore.xFile,
+//         el->mvt->xyzBefore.yRank,
+//         el->mvt->xyzBefore.zLevel,
+//         el->mvt->xyzAfter.xFile,
+//         el->mvt->xyzAfter.yRank,
+//         el->mvt->xyzAfter.zLevel);
+//  el = el->below;
+//}
+//}
+//#endif /* DEBUG */
+
+	
+
+
+
+
+
 //	
 //	
 //	/*
