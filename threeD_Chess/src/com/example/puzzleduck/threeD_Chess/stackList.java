c8199d0d398730539ca087307cfa32f0ca068f53
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2011-01-13 09:26:17 +0000

    implemented PushMove

--- threeD_Chess/src/com/example/puzzleduck/threeD_Chess/stackList.java
+++ threeD_Chess/src/com/example/puzzleduck/threeD_Chess/stackList.java
@@ -8,20 +8,16 @@ public class stackList {
 //	{
 //	  stack *stacks[BEST_STACKS];
 //	  int   ratings[BEST_STACKS];
-//	} bestMoves = {
-//	{ NULL, NULL, NULL, NULL, NULL },
-//	{ INT_MIN, INT_MIN, INT_MIN, INT_MIN, INT_MIN }
-//	};
+//	} 
 //
 //	#define BEST_STACKS 5
-	public int BEST_STACKS = 5;
 
 public Stack stacks[] = {null,null,null,null,null};
 public int   ratings[] = {0,0,0,0,0};
 	
-//	public stackList()
-//	{
-////		stacks = ;
-//	}
+	public stackList()
+	{
+////		stacks = default values;
+	}
 	
 }
