73dd92922c2b2739e2b6dea2f2dbc49a4da59102
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2011-05-19 13:45:39 +0000

    ok... resolved conflicts and hopefully fixed broken repo.

--- threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Board.java
+++ threeD_Chess/src/com/example/puzzleduck/threeD_Chess/Board.java
@@ -5,7 +5,6 @@ import java.util.Stack;
 public class Board {
 	private Stack MoveStack;
 	private Piece[][][] Board;
-	boolean[][][] targets;
 	private Piece[][] Muster;
 	private int bwToMove;
 	private Piece SQUARE_EMPTY;
@@ -16,19 +15,6 @@ public class Board {
 		Board = board;
 		Muster = muster;
 		TitleCount = titleCount;
-		
-		targets = new boolean[threeD_Chess.LEVELS][threeD_Chess.RANKS][threeD_Chess.FILES];
-		
-//		for(int i = --threeD_Chess.LEVELS; i >= 0; i--)
-//		{
-//			for(int j = --threeD_Chess.RANKS; j >= 0; j--)
-//			{
-//				for(int k = --threeD_Chess.FILES; k >= 0; k--)
-//				{
-//					targets[i][j][k] = false;
-//				}
-//			}
-//		}
 	}
 
 	public Stack getMoveStack() {
@@ -90,10 +76,5 @@ public class Board {
 	public Piece getPieceAt(int i, int j, int k) {
 		// TODO Auto-generated method stub
 		return Board[i][j][k];
-	}
-
-	public void setTarget(int xFile, int yRank, int zLevel, boolean on) {
-		targets[xFile][yRank][zLevel] = on;
-		
 	}
 }
