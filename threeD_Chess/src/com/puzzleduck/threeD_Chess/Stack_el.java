3c9ebee7a494d5dbc53f61527801e4bcb51f04a4
puzzleduck <puzzleduck@4e8d1580-0054-77ce-f07d-b29e0aa09d36>
2012-10-29 08:45:15 +0000

    remove stack el

--- threeD_Chess/src/com/puzzleduck/threeD_Chess/Stack_el.java
+++ threeD_Chess/src/com/puzzleduck/threeD_Chess/Stack_el.java
@@ -1,26 +1,26 @@
 package com.puzzleduck.threeD_Chess;
 
-public class Stack_el {
-
-
-	//
-	//stack has its own class... but may only need wrappers???
-	//reappropriating for stack_el
-
-	//moving stack item to new class
-	//	struct stack_el
-	//	{
-	//	  Move *mvt;
-	//	  struct stack_el *below;
-	//	};
-
-	public Move mvt;
-	//	public Stack_el below;
-	public Stack_el below;
-
-	public Stack_el(){
-		mvt = null;
-		below = null;
-	}
-
-}
+//public class Stack_el {
+//
+//
+//	//
+//	//stack has its own class... but may only need wrappers???
+//	//reappropriating for stack_el
+//
+//	//moving stack item to new class
+//	//	struct stack_el
+//	//	{
+//	//	  Move *mvt;
+//	//	  struct stack_el *below;
+//	//	};
+//
+//	public Move mvt;
+//	//	public Stack_el below;
+//	public Stack_el below;
+//
+//	public Stack_el(){
+//		mvt = null;
+//		below = null;
+//	}
+//
+//}
